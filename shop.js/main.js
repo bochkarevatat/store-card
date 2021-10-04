3bcb57a66d66dc17e0cf12f13bec237cb049ee71
bochkarevatat <tanitabt@rambler.ru>
2021-10-04 14:03:40 +0400

    Finished js

--- shop.js/main.js
+++ shop.js/main.js
@@ -8,11 +8,76 @@ sidebarToggleBtn.onclick = function () {
 	menuIcon.classList.toggle('menu-icon-active');
 	sidebar.classList.toggle('sidebar--mobile-active');
 };
-
+// Кнопка, котрая скрывает карточки
 const btnShoreMoreCards = document.querySelector('.show_more');
-const openShoreMore = document.querySelector('.hidden-cards');
+const openShoreMore = document.querySelectorAll('.hidden-cards');
 
 
-btnShoreMoreCards.addEventListener("click", function (){
-    openShoreMore.classList.remove('hidden-cards');
+btnShoreMoreCards.addEventListener("click", function () {
+	openShoreMore.forEach(function (card) {
+		card.classList.remove('hidden-cards');
+	})
+
+});
+
+// Кнопка, которая скрывает виджеты
+const vidgetAll = document.querySelectorAll('.vidget');
+
+vidgetAll.forEach(function (vidget) {
+	vidget.addEventListener('click', function (e) {
+		if (e.target.classList.contains('vidget-title')) {
+			e.target.classList.toggle('vidget-title--active');
+			e.target.nextElementSibling.classList.toggle('vidget-body');
+		}
+	});
+});
+
+// Кнопка, которая работает с "Любое"
+
+const checkBoxAny = document.querySelector('#location_05');
+const topCheckboxes = document.querySelectorAll('.location-activ');
+
+
+checkBoxAny.addEventListener('change', function () {
+	if (checkBoxAny.checked) {
+		topCheckboxes.forEach(function (item) {
+			item.checked = false;
+		});
+	}
 })
+
+
+topCheckboxes.forEach(function (item) {
+	item.addEventListener('change', function () {
+		if (checkBoxAny.checked) {
+			checkBoxAny.checked = false;
+		}
+	})
+})
+
+const checkboxShowMore = document.querySelector('.btn-shore_more');
+const hidenCheckbox = document.querySelectorAll('.checkbox-hiden');
+
+
+checkboxShowMore.onclick = function (e) {
+
+	e.preventDefault();
+
+	// показываем
+	if (checkboxShowMore.dataset.options == 'hidden') {
+		hidenCheckbox.forEach(function (item) {
+			item.style.display = 'block';
+		});
+		checkboxShowMore.innerText = 'Скрыть дополнительные опции';
+		checkboxShowMore.dataset.options = 'visible';
+	}
+	//  скрываем
+	else if (checkboxShowMore.dataset.options == 'visible') {
+		hidenCheckbox.forEach(function (item) {
+			item.style.display = 'none';
+		});
+		checkboxShowMore.innerText = 'Показать ещё';
+		checkboxShowMore.dataset.options = 'hidden';
+	}
+
+}
